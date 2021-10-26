a23e4a4e8e257e8d0a464a235d8f2ac1d88102ce
Nayara Nogueira Escudeiro do Nascimento <nanaescudeiro2015@gmail.com>
2021-10-25 22:51:56 -0300

    eita bbs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Usuarios extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listaRepositorios: [],
+      nomeRepositorio: ''
+    }
+  }
+  buscarRepositorios = (elemento) => {
+    elemento.preventDefault();
+    console.log("Estamos procurando!")
+    fetch(`https://api.github.com/users/${this.state.nomeRepositorio}/repos?per_page=10&sort=author-date-desc`)
+
+      .then(resposta => resposta.json())
+
+      .then(lista => this.setState({ listaRepositorios: lista }))
+
+      .catch(erro => console.log(erro))
+  }
+
+  atualizaNome = async (nome) => {
+    await this.setState({ nomeRepositorio: nome.target.value })
+    console.log(this.state.nomeRepositorio)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <main>
+          <section>
+            <h2> Procure os usuários do Github </h2>
+            <form onSubmit={this.buscarRepositorios}>
+              <div>
+                <input
+                  type="text"
+                  value={this.state.nomeRepositorio}
+                  onChange={this.atualizaNome}
+                  placeholder="Insira o nome do usuário"
+                />
+                <button type="submit" onClick={this.buscarRepositorios}> Buscar </button>
+              </div>
+            </form>
+          </section>
+          <table>
+            <thead>
+              <th> ID </th>
+              <th> NOME </th>
+              <th> DESCRIÇÃO </th>
+              <th> DATA DE CRIAÇÃO </th>
+              <th> TAMANHO </th>
+            </thead>
+
+            <tbody>
+              {this.state.listaRepositorios.map((repositorio) => {
+                return (
+                  <tr key={repositorio.id}>
+                    <td>{repositorio.id}</td>
+                    <td>{repositorio.name}</td>
+                    <td>{repositorio.description}</td>
+                    <td>{repositorio.created_at}</td>
+                    <td>{repositorio.size}</td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        </main>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default Usuarios;
